74dbcdfdb0b0e410cac6311737f2257cd47eef1d
desktop-d7dqtnr\adria <adrboo2001@skola.goteborg.se>
2020-10-13 15:15:25 +0200

    new function fak() in HelloGit.java

--- HelloGit.java
+++ HelloGit.java
@@ -1,5 +1,13 @@
 public class HelloGit {
+    public static int fak(int x) {
+            int result;
+            if (x==1) {
+                return 1;
+            }
+            result = fak(x-1)*x;
+            return result;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git! "+fak(4));
     }
 }
